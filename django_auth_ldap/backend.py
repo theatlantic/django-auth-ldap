0af74fc31042b84905be84e0ab796624d6f01b45
David Krauth <dkrauth@theatlantic.com>
2020-07-10 15:16:52 -1000

    Remove unwanted commented code

--- django_auth_ldap/backend.py
+++ django_auth_ldap/backend.py
@@ -778,7 +778,6 @@ class _LDAPUser(object):
             new_groups = [Group.objects.get_or_create(name=name)[0] for name
                           in target_group_names if name not in existing_group_names]
 
-            # self._user.groups = existing_groups + new_groups
             self._user.groups.set(existing_groups + new_groups)
 
     #
